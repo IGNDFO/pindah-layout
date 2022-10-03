608b532b117eab336c9ff0e9b4e717baf901a829
oriell754 <fernand0753@mhs.mdp.ac.id>
2022-10-03 10:49:55 +0700

    pindah

--- app/src/main/java/com/example/pindahlayout/MainActivity.java
+++ app/src/main/java/com/example/pindahlayout/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.pindahlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button Btnpindah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Btnpindah=findViewById(R.id.btn_pindah);
+        Btnpindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent pindah= new Intent(MainActivity.this, secondActivity.class);
+                startActivity(pindah);
+                Intent p= new Intent(MainActivity.this,secondActivity.class);
+                startActivity(p);
+            }
+            
+        });
     }
 }
